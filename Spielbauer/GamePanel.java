20ec1c984d5a2fd2e2899da9d9923600e5f8fcee
lm3333333 <luca.muehlbacher08@htl-salzburg.ac.at>
2025-12-04 07:55:07 +0100

    only heligame code

--- Spielbauer/GamePanel.java
+++ Spielbauer/GamePanel.java
@@ -7,8 +7,15 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.util.Vector;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 //implements Runnable, KeyListener
-public class GamePanel extends JPanel implements Runnable {
+public class GamePanel extends JPanel implements Runnable, KeyListener {
     
     private static final long serialVersionUID = 1L;
     boolean game_running = true;
@@ -16,6 +23,15 @@ public class GamePanel extends JPanel implements Runnable {
     long delta = 0;
     long last = 0;
     long fps = 0;
+
+    Sprite copter;
+    Vector<Sprite> actors;
+
+    boolean up = false;
+    boolean down = false;
+    boolean left = false;
+    boolean right = false;
+    int speed = 50;
 
     public static void main(String[] args) {
         new GamePanel(800,600);
@@ -27,6 +43,7 @@ public class GamePanel extends JPanel implements Runnable {
         JFrame frame = new JFrame("GameDemo");
         frame.setLocation(100, 100);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.addKeyListener(this);
         frame.add(this);
         frame.pack();
         frame.setVisible(true);
@@ -36,6 +53,11 @@ public class GamePanel extends JPanel implements Runnable {
     private void doInitializations() {
 
         last = System.nanoTime();
+
+        actors = new Vector<Sprite>();
+        BufferedImage[] heli = this.loadPics("pics/heli.gif", 4);
+        copter = new Sprite(heli, 100, 100, 100, this);
+        actors.add(copter);
 
         Thread t = new Thread(this);
         t.start();
@@ -71,18 +93,72 @@ public class GamePanel extends JPanel implements Runnable {
         fps = ((long) 1e9) /  delta;
     }
 
+    private void checkKeys() {
+        // placeholder: implement keyboard handling here
+    }
+
+    private void doLogic() {
+        for (Movable move:actors) {
+            move.doLogic(delta);
+        }
+    }
+
+    private void moveObjects() {
+        for (Movable move:actors) {
+            move.move(delta);
+        }
+    }
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         g.setColor(Color.red);
         g.drawString("FPS: " + Long.toString(fps), 20, 10);
+
+        if(actors != null) {
+            for (Drawable draw:actors) {
+                draw.drawObjects(g);
+            }
+        }
     }  
 
+    private BufferedImage[] loadPics(String path, int pics) {
 
+        BufferedImage[] anim = new BufferedImage[pics];
+        BufferedImage source = null;
+
+        URL pic_url = getClass().getClassLoader().getResource(path);
+
+        try {
+            source = ImageIO.read(pic_url); 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int x=0;x<pics;x++) {
+            anim[x] = source.getSubimage(x*source.getWidth()/pics, 0, 
+            source.getWidth()/pics, source.getHeight());
+        }
+        return anim;
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+ 
 }
 
 
 
 
-
+/// Page 25 
--- Spielbauer/Sprite.java
+++ Spielbauer/Sprite.java
@@ -1,6 +1,7 @@
 package BrotatoClone.Spielbauer;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.awt.geom.Rectangle2D;
 
 public class Sprite extends Rectangle2D.Double implements Drawable, Movable {
@@ -10,6 +11,8 @@ public class Sprite extends Rectangle2D.Double implements Drawable, Movable {
     GamePanel parent;
     BufferedImage[] pics;
     int currentpic = 0;
+    protected double dx;
+    protected double dy;
 
     public Sprite(BufferedImage[] i, double x, double y, long delay, GamePanel p) {
 
@@ -24,15 +27,55 @@ public class Sprite extends Rectangle2D.Double implements Drawable, Movable {
     }
 
     public void drawObjects(Graphics g) {
-        
+        if (pics == null || pics.length == 0) return;
+        g.drawImage(pics[currentpic], (int) this.x, (int) this.y, null);
     }
 
     public void doLogic(long delta) {
         
+        animation += (delta/1000000);
+        if (animation > delay) {
+            animation = 0;
+            computerAnimation();
+        }
     }
 
-    public void move(long delta) {
-        
+    private void computerAnimation() {
+
+        currentpic++;
+
+        if(currentpic>=pics.length) {
+            currentpic = 0;
+        }
+    }
+
+    public void setVerticalSpeed(double d) {
+        dy = d;
+    }
+
+    public void setHorizontalSpeed(double d) {
+        dx = d;
+    }
+
+    public double getVerticalSpeed() {
+        return dy;
+    }
+
+    public double getHorizontalSpeed() {
+        return dx;
+    }
+
+    public void move(long Delta) {
+
+        if(dx != 0) {
+            x += dx * (Delta / 1e9);
+        }
+
+        if(dy != 0) {
+            y += dy * (Delta / 1e9);
+        }
     }
     
 }
+
+/// Page 17 stoped 
